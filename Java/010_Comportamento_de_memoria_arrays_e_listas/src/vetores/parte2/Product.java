03d58565f331cd6a2b52cf8da5fc35f0e3c023f4
Gabriel Henrique <82977458+GabrielHenriqueHE@users.noreply.github.com>
2024-07-25 22:51:48 -0300

    Estudando

---
+++ Java/010_Comportamento_de_memoria_arrays_e_listas/src/vetores/parte2/Product.java
@@ -0,0 +1,12 @@
+package vetores.parte2;
+
+public class Product {
+    String name;
+    double price;
+
+    public Product (String name, double price) {
+        this.name = name;
+        this.price = price;
+    }
+
+}
