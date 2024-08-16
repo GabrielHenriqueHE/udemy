49ef93d79aa2852b691a93f5ff4c9227497f791a
Gabriel Henrique <82977458+GabrielHenriqueHE@users.noreply.github.com>
2024-08-16 14:21:26 -0300

    Excluindo o projeto para dar continuidade mais a frente

--- Java/016_projeto_sistema_jogo_de_xadrez/src/board/BoardException.java
+++
@@ -1,9 +0,0 @@
-package board;
-
-public class BoardException extends RuntimeException {
-    private static final long serialVerisonUID = 1L;
-
-    public BoardException(String message) {
-        super(message);
-    }
-}
